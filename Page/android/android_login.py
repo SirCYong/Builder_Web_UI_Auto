41b11c6259a35bf031fa94be1e43842000256831
SirCYong <1098041066@qq.com>
2017-12-19 10:58:32 +0800

    delete file

--- Page/android/android_login.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-# auth:cy
-from Page.Element import get_element
-
-
-def android_login(driver, username, password):
-    get_element(driver, ('xpath', "//android.widget.EditText[@text='请输入帐号']")).send_keys(username)
-    get_element(driver, ('xpath', "//android.widget.EditText[@text='••••••••••']")).send_keys(password)
-    get_element(driver, ('ACCESSIBILITY_ID', "登录 ")).click()
